435c36f07c54b6b51bad1b4e1f9faf5b8dee642f
Joshan Rai <pradheon@gmail.com>
2022-02-22 18:32:02 -0800

    Initial Commit

---
+++ JoshanRai-WordScramble/JoshanRai_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_WordScrambleApp.swift
+//  JoshanRai-WordScramble
+//
+//  Created by Joshan Rai on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

b47c16e53686fd59ffc7b0144cb3ae5b2875055e
Joshan Rai <pradheon@gmail.com>
2022-02-22 18:41:55 -0800

    v0.1 - Dynamic and static views with List

--- JoshanRai-WordScramble/ContentView.swift
+++ JoshanRai-WordScramble/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    //let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            Section {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+            
+            ForEach(0..<5) {
+                Text("Dynamic row \($0)")
+            }
+            
+            Section {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
+        }
+        .listStyle(.grouped)
     }
 }
 
